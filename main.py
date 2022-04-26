6b0dd3dfe7a439b2f6d0019b9dff4440c9687be9
Anton Barycheuski <antofa1@mail.ru>
2022-04-26 23:26:36 +0300

    up

--- main.py
+++ main.py
@@ -7,7 +7,7 @@
 from time import sleep
 import sys
 import webbrowser
-from PyQt5.QtWidgets import QWidget, QApplication, QHBoxLayout, QVBoxLayout, QPushButton, QLabel, QLineEdit
+from PyQt5.QtWidgets import QWidget, QApplication, QHBoxLayout, QVBoxLayout, QPushButton, QLabel, QLineEdit, QSizePolicy
 from PyQt5 import QtCore
 from PyQt5.QtCore import Qt, QPoint
 from PyQt5.QtGui import QFont, QPalette, QColor, QIcon
@@ -46,6 +46,217 @@ eventSolverFilePath = f'{localLowPath}{guLogPath}/event_solver/event_solver_info
 playerInfoFilePath = f'{localLowPath}{guLogPath}/player/player_info.txt'
 # gameNetworkManagerFilePath = f'{localLowPath}{guLogPath}/GameNetworkManager/GameNetworkManager_info.txt'
 outputLogSimpleFilePath = f'{localLowPath}{guLogPath}/../../output_log_simple.txt'
+
+HTML_TEMPLATE = '''
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Home page</title>
+    <style type="text/css">
+body {
+    background-color: #1d1d1d;
+    display: flex;
+    flex-direction: row;
+    justify-content: space-between;
+    color: #fff;
+    font-family: Consolas;
+}
+
+.hidden {
+    display: none;
+}
+
+
+.common, .common:hover{
+    color: #e6e6e6;
+}
+.common-background{
+    background-color: #e6e6e6;
+}
+.common-word-color, .common-word-color:hover{
+    color: #bdb988;
+}
+.rare, .rare:hover{
+    color: #42a5f5;
+}
+.rare-background{
+    background-color: #42a5f5;
+}
+.epic, .epic:hover{
+    color: #ba68c8;
+}
+.epic-background{
+    background-color: #ba68c8;
+}
+.legendary, .legendary:hover{
+    color: #ffca28;
+}
+.legendary-background{
+    background-color: #ffca28;
+}
+.mythic, .mythic:hover{
+    color: #ef5350
+}
+.mythic-background{
+    background-color: #ef5350
+}
+
+
+#deck-list-god-image-area{
+    display: inline-block;
+    margin-top: 10px;
+    width: 240px;
+    height: 50px;
+    background-position: 0% -80%;
+    background-size: cover;
+    border: 3px solid #505050;
+    border-radius: 6px;
+}
+
+#deck-list{
+    display: flex;
+    flex-direction: column;
+    align-items: center;
+    justify-content: center;
+}
+
+#deck-list:not(:last-child){
+    margin-bottom: 15px;
+}
+
+.deck-list-item-wrapper{
+ display: flex;
+ align-items: center;
+}
+
+.deck-list-item-wrapper:not(:last-child){
+    margin-bottom: 2px;
+}
+
+.deck-list-item{
+    display : flex;
+    justify-content: center;
+    align-items: center;
+    color:white;
+    font-size: 1.3em;
+    cursor: pointer;
+    width: fit-content;
+}
+.deck-list-item-unowned{
+    opacity: .5;
+}
+
+.deck-list-item:hover {
+    filter: brightness(1.2);
+}
+
+.deck-list-item-name-border{
+    display: flex;
+    font-size: .7em;
+    width: 250px;
+    border: 1px solid rgb(80, 80, 80);
+    position: relative;
+    padding: 5px;
+}
+.god-power-list-item-name-border{
+    width: 10.5em;
+    border: 2px solid rgb(185, 161, 94);
+    position: relative;
+    padding: 0 0 0 1.3em;
+    border-radius: 6px;
+}
+.deck-list-item-background{
+    position: absolute;
+    background-size: cover;
+    background-position-y: 20%;
+    opacity: .9;
+    top: 0;
+    left: 45%;
+    bottom: 0;
+    right: 5%;
+    z-index: 0;
+}
+.deck-list-item-background-unowned{
+    filter:saturate(.5);
+}
+
+.deck-list-item-background-fade{
+    position: absolute;
+    background-image: linear-gradient(to right, #1d1d1d, #1d1d1d00);
+    top: 0;
+    left: 45%;
+    bottom: 0;
+    right: 30%;
+    z-index: 0;
+}
+.deck-list-item-background-fade-right{
+    position: absolute;
+    background-image: linear-gradient(to right, #1d1d1d00, #1d1d1d);
+    top: 0;
+    left: 78%;
+    bottom: 0;
+    right: 4.5%;
+    z-index: 0;
+}
+.deck-list-item-name{
+    text-overflow: ellipsis;
+    /* required for ellipsis */
+    overflow: hidden;
+    white-space: nowrap;
+    text-align: left;
+    flex-grow: 1;
+    z-index: 1;
+}
+.deck-list-item-count{
+    margin-right: 15px;
+    z-index: 1;
+}
+.deck-list-item-rarity-strip{
+    position: absolute;
+    top: 0;
+    left: 98%;
+    bottom: 0;
+    right: 0;
+    z-index: 0;
+}
+
+.deck-list-item-percentage{
+  margin-left: 10px;
+  color:darkgrey;
+ }
+
+
+
+div.tooltip img {
+  z-index:10;
+  display:none;
+  margin-top:5px;
+  background: gray;
+}
+div.tooltip:hover img{
+    display:inline; position:absolute; 
+    top: 0px;
+    background: transparent;
+}
+div.tooltip-left:hover img{
+    right: 0px;
+}
+div.tooltip-right:hover img{
+    left: 0px;
+}
+    </style>
+</head>
+<body>
+    <div class="me">
+        [me_DECKS]
+    </div>
+    <div class="opponent">
+        [opponent_DECKS]
+    </div>
+</body>
+</html>
+'''
 
 #########################
 # PyInstaller Functions #
@@ -413,11 +624,15 @@ class MainWindow(QWidget):
         # Creation of the Main Window #
         ###############################
 
-        self.setWindowFlags(QtCore.Qt.WindowStaysOnTopHint | QtCore.Qt.FramelessWindowHint)
+        # self.setWindowFlags(QtCore.Qt.WindowStaysOnTopHint | QtCore.Qt.FramelessWindowHint)
+        self.setWindowFlags(QtCore.Qt.WindowStaysOnTopHint)
         self.tempWindow = None
         self.layout = QVBoxLayout()
         self.setWindowTitle(windowTitle)
         self.setWindowIcon(QIcon(windowIcon))
+        self.setSizePolicy(
+            QSizePolicy.Expanding, QSizePolicy.Expanding
+        )
 
         buttonSize = (30, 30)
         iconSize = 12
@@ -469,6 +684,13 @@ class MainWindow(QWidget):
 
         self.webEngineView = QWebEngineView()
         self.webEngineView.setHtml('<div>hello</div>')
+        self.webEngineView.setSizePolicy(QSizePolicy.Maximum, QSizePolicy.Maximum)
+        self.webEngineView.setMinimumSize(430, 800)
+        self.webEngineView.setZoomFactor(0.9)
+        print(self.sizeHint())
+        print(self.webEngineView.sizeHint())
+        print(self.webEngineView.size())
+        self.webEngineView.resize(self.size())
         # self.webEngineView.setWindowFlags(Qt.FramelessWindowHint)
         # self.webEngineView.setAttribute(Qt.WA_TranslucentBackground, True)
         self.layout.addWidget(self.webEngineView)
@@ -555,8 +777,11 @@ class MainWindow(QWidget):
             #     # Event solver doesn't exist yet, so just wait
             #     return
 
-            decksText = getDecksStr()
-            decksHtml = player.asHtml()
+            # decksText = getDecksStr()
+            meHtml = player.asHtml()
+            opponentHtml = opponent.asHtml()
+            decksHtml = HTML_TEMPLATE.replace(f'[me_DECKS]', meHtml).replace(f'[opponent_DECKS]', opponentHtml)
+
             open('my_deck.html', 'wb').write(decksHtml.encode('utf8'))
 
         self.deckTrackerLabel.setText(decksText)
@@ -572,7 +797,7 @@ class MainWindow(QWidget):
             self.layout.removeWidget(self.deckTrackerLabel)
 
         # self.setLayout(self.layout)
-        self.adjustSize()
+        # self.adjustSize()
 
     def settings(self):
         self.tempWindow = SettingsWindow(self.windowTitle, self.configFile)
